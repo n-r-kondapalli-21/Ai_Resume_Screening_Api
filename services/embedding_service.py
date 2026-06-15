281d25a136ffd25d0a1f9c0bfa6036682dae7046
Narayanarao Kondapalli <narayanakondapalli148@gmail.com>
2026-06-15 16:33:14 +0530

    embedding service

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -1,15 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-model = None
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def get_model():
-    global model
-    if model is None:
-        model = SentenceTransformer(
-            "sentence-transformers/all-MiniLM-L6-v2"
-        )
-    return model
 
 def generate_embedding(text):
-    model = get_model()
-    return model.encode(text)
+
+    embedding = model.encode(text)
+
+    return embedding
